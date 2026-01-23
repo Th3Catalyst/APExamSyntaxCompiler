93bf7f7fd4ccf8fea94eaf7f8b8192c686dacbc8
5Stygian <121655781+5Stygian@users.noreply.github.com>
2026-01-23 10:12:33 -0800

    fix barp level bad practice

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <istream>
-using namespace std;
 
-int main() {
-
-  
-    ifstream in("main.apexam");     
+int main()
+{
+    std::ifstream in("main.apexam");     
 
     while(!in.eof())
     {
-    string text;
-    istream getline(in, text);
-
+        std::string text;
+        std::istream getline(in, text);
     }
 
     return 0;
