4059065c5e7769b6869fdae5fa215c05094f8843
5Stygian <121655781+5Stygian@users.noreply.github.com>
2026-01-23 10:55:09 -0800

    refactor code to follow bamon level best practice

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <istream>
+#include <filesystem>
 
 int main()
 {
-    std::ifstream in("main.apexam");     
+    std::string fileContents;
+    std::ifstream in("test.apexam");     
+
+    while (getline(in, fileContents)) {
+        std::cout << fileContents << "\n";
+    }
 
     while(!in.eof())
     {
-        std::string text;
-        std::istream getline(in, text);
+        //std::string text;
+        //std::istream getline(in, text);
     }
+
+    in.close();
 
     return 0;
 } 
